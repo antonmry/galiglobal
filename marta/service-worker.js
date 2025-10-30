95540dd9969a7757408fb91787b135475bb58483
Anton Rodriguez <anton@galiglobal.com>
2025-10-30 17:20:07 +0100

    Add minimal PWA support

---
+++ marta/service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'marta-intervals-v1';
+const OFFLINE_URLS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './hrm.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(OFFLINE_URLS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request).then(response => {
+        const cloned = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, cloned);
+        });
+        return response;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
